c3782b84a81121965562925b79a873a3a4bfdf99
“Ratha <mrrrathanj@gmail.com>
2024-07-22 22:57:02 +0700

    config application

---
+++ services/customer/src/test/java/com/quicksoft/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quicksoft.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
